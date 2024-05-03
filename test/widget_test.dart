2a75869142edc0b7b11d4efd48cbc142a8780d2b
ErosM04 <eros.menin@gmail.com>
2024-05-03 21:30:05 +0200

    corrected wrong name in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:link4launches/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const _Link4Launches());
+    await tester.pumpWidget(const Link4Launches());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
